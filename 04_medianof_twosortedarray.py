5dcd212d655240f501de6f71f667bb554dfcf65a
Arunesh Singh <125174712+arunesh-singhh@users.noreply.github.com>
2023-07-02 14:05:23 +0530

    Update 04_medianof_twosortedarray.py
    approach 2

--- 04_medianof_twosortedarray.py
+++ 04_medianof_twosortedarray.py
@@ -10,3 +10,35 @@ class Solution:
             return (nums3[half-1] + nums3[half]) / 2
         else:
             return nums3[half-1]
+
+
+#approach-2
+class Solution:
+    def findMedianSortedArrays(self, num1: List[int], num2: List[int]) -> float:
+        x = len(num1)
+        y = len(num2)
+
+        if x > y:
+            return self.findMedianSortedArrays(num2, num1)
+        
+        low = 0
+        high = x
+
+        while low <= high:
+            px = (low + high)//2
+            py = ((x + y + 1)//2) - px
+
+            maxlx = float("-inf") if px == 0 else num1[px-1]
+            minrx = float("-inf") if px == x else num1[px]
+            maxly = float("-inf") if py == 0 else num2[py-1]
+            minry = float("-inf") if py == y else num2[py]
+
+            if maxlx <= minry and maxly <= minrx:
+                if (x + y) % 2 == 0:
+                    return(max(maxlx, maxly) + min(minrx, minry)) / 2
+                else:
+                    return(max(maxlx, maxly))
+            elif maxlx > minry:
+                high = px - 1
+            else:
+                low = px + 1
